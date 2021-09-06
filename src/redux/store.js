223e74d76cc730c53332dc22736d34ee155ec1b4
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-06 14:53:45 +0100

    Wired redux thunk for async actions.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducers, composeEnhancers());
+export default createStore(reducers, composeEnhancers(reduxThunk));
